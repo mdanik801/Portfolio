360f67dbe4f5ff4dbe8feec2a76dd5daaeca1aca
Md Aulad Hossain Anik <122743454+mdanik801@users.noreply.github.com>
2024-07-15 07:29:31 +0600

    first commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -163,8 +163,8 @@ export default function Contact() {
                      <>
                         {" "}
                         <button className={style.sendbtn}>Send Massage</button>
-                        <span className=" text-green-600 text-shadow-lg font-Nunito font-bold">
-                           {suess}
+                        <span className=" text-green-600 text-shadow-lg font-Nunito font-bold lg:text-[1rem] text-[0.8rem]">
+                           {suess}Email sent successfully!
                         </span>
                      </>
                   )}{" "}
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -11,8 +11,8 @@ const style = {
    skill_itemsection: ` h-auto  mt-[2vh] flex justify-start items-start`,
    skill_name: `flex  flex-col items-start w-auto   `,
    skill_name_h: `w-[20vh] lg:mx-2 mx-1 text-white font-Nunito lg:text-[1rem] text-[0.8rem] text-shadow-lg  `,
-   skill_detail_box: `flex flex-wrap  justify-start items-center mt-[0.5vh]`,
-   skill_btn: `lg:text-[0.9rem] text-[0.4rem] text-shadow-lg p-1 font-Nunito  text-slate-300 flex justify-between items-center rounded-lg bg-slate-700 hover:bg-slate-800 duration-200 m-[0.6vh] border border-green-400 shadow-md shadow-black`,
+   skill_detail_box: `flex flex-wrap  justify-start items-center  mt-[0.5vh]`,
+   skill_btn: `lg:text-[0.9rem] text-[0.4rem] lg:w-auto w-[10vh] text-shadow-lg p-1 font-Nunito  text-slate-300 flex justify-between items-center rounded-lg bg-slate-700 hover:bg-slate-800 duration-200 m-[0.6vh] border border-green-400 shadow-md shadow-black`,
    skill_icon: ` lg:w-[4vh] w-[3vh] mr-1`,
 };
 
