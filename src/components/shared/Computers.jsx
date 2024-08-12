0e3c8004478819ab498ade7a843b49922b2f3ba7
Md Aulad Hossain Anik <122743454+mdanik801@users.noreply.github.com>
2024-08-12 12:43:38 +0600

    lodare

--- src/components/shared/Computers.jsx
+++ src/components/shared/Computers.jsx
@@ -1,6 +1,7 @@
 import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import CanvasLoader from "./Loader";
 
 // import CanvasLoader from "../Loader";
 
@@ -63,7 +64,7 @@ const ComputersCanvas = () => {
          dpr={[1, 2]}
          camera={{ position: [20, 3, 5], fov: 25 }}
          gl={{ preserveDrawingBuffer: true }}>
-         <Suspense>
+         <Suspense fallback={<CanvasLoader />}>
             <OrbitControls
                enableZoom={false}
                maxPolarAngle={Math.PI / 2}
